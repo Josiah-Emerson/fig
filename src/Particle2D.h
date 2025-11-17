4096f5ada23299e55386d61af0ddef48db7c6912
Josiah Emerson <e.a.josiah@gmail.com>
2025-11-16 19:56:36 -0800

    Wrote tests for Particle2D class

--- src/Particle2D.h
+++ src/Particle2D.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <stdexcept>
 
 class Particle2D{
    public: 
       struct Position2D{
          double x { };
          double y { };
+
+         bool operator==(const Position2D& rhs) const { return x == rhs.x && y == rhs.y; }
+         bool operator!=(const Position2D& rhs) const { return !(*this==rhs);}
       };
 
    private: 
@@ -15,7 +19,10 @@ class Particle2D{
       Particle2D(double radius, Position2D position = { 0, 0 } )
          : radius_ { radius }
          , position_ { position }
-         {}
+         {
+            if(radius_ <= 0)
+               throw std::invalid_argument("Radius must be greater than 0");
+         }
 
       double radius() const { return radius_; }
       Position2D position() const { return position_; }
