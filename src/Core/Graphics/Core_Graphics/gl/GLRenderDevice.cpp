e0bb18a1e609ea7ba7456bccd267a968cccc7660
Josiah Emerson <e.a.josiah@gmail.com>
2026-05-13 15:27:24 -0700

    Removed some comments which were left over from experimenting

--- src/Core/Graphics/Core_Graphics/gl/GLRenderDevice.cpp
+++ src/Core/Graphics/Core_Graphics/gl/GLRenderDevice.cpp
@@ -82,11 +82,6 @@ namespace Core{
             currBoundModel = u.model;
          }
 
-         // TODO: This is temp for when we just have a position component
-         // TODO: Make it more automated
-         // Create a struct for unpacking the ComponentList
-         // Track per component: ComponentPool, start index, bool for if it exists
-         // Track: A pointer to first element here and then go for the offset 
          using ArgHelperType = typename CreateComponentFunction<GraphicsComponentList>::ArgType;
          ArgHelperType components {};
          
@@ -125,77 +120,6 @@ namespace Core{
             m_openGL.glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
          }
       }
-
-      // Get an iterator for ShaderProgram and Model
-      // Use Program
-      // For each ShaderProgram and model combo set the uniform (Assume it is MVP)
-
-      // TODO: how do we pass the necessary values here? 
-      // Example: for the MVP matrix we could pass the camera through the function here, 
-      // (cuz we def don't want it to be owned by RenderDevice), but if different programs 
-      // require different things, we probably need to inject something 
-      // For testing just create a camera here I guess
-
-
-      /*
-      auto V = camera.viewMatrix();
-      auto P = camera.projectionMatrix();
-
-      auto positionPool = m_registry.getPool<PositionComponent>();
-      const auto& iterators = positionPool.separatorList();
-      std::shared_ptr<ShaderProgram> currBoundProgram { nullptr };
-      std::shared_ptr<Model> currBoundModel { nullptr };
-
-      // TODO: depending on final form, look at hoisting out commonly looked at vars 
-      // into a local var for better cache
-      for(const auto& it : iterators){
-         const GraphicsComperand& gComp = it.first;
-         const auto& separator = it.second;
-         // If ShaderProgram in iterator changes, rebind 
-
-         if(gComp.prog != currBoundProgram){
-            assert(gComp.prog->useProgram() && "Error using program");
-            currBoundProgram = gComp.prog;
-         }
-
-         if(gComp.model != currBoundModel){
-            auto search = std::find_if(m_modelVAOList.begin(), m_modelVAOList.end(), 
-                  [&gComp](const std::pair<const Model&, GLuint>& v){
-                     return v.first == *gComp.model;
-                  });
-
-            assert(search != m_modelVAOList.end() && "model in pool not found in RenderDevice");
-            m_openGL.glBindVertexArray(search->second);
-
-            currBoundModel = gComp.model;
-         }
-
-         //                     [first, second]
-         // <= cuz separator is [lower, upper ] inclusive
-         for(std::size_t i { separator.first }; i <= separator.second; ++i){
-            // build MVP matrix here
-            // MVP = P * V * M
-            auto val = positionPool[i];
-
-            // Example: 1 ShaderProgram has MVP uniform and one has MVP and COLOR uniform
-            // MVP is mat4 in both and COLOR is vec3. How do we go about so that it knows 
-            // which one, and how to use it 
-            // an entity using program 1 would have an entry in position pool which could be used to calculate
-            // MVP, while an entity using program 2 would have that AND an entry in colorPool 
-            // with its color value
-            // in this case every shaderProgram would have an associated set of pools with the 
-            // data it needs and how to transform that data? 
-
-            auto M = Linear::modelMatrix(val, Linear::Vector{1, 1, 1});
-            auto MVP = P * V * M;
-            // auto MVP_T = MVP.transpose();
-            assert(currBoundProgram->setUniform("MVP", &MVP, F_MAT4) && "Could not setUniform");
-
-            m_openGL.glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // NOTE: look how hardcoded this is, must be a better way
-         }
-         
-      }
-   */
    }
 
    void* GLRenderDevice::getDataPtr(ShaderDataType type){
